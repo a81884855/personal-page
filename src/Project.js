c89df460fda0e3d41a09e7f97129764b101f345a
Gary <garyguan0713@gmail.com>
2020-06-23 17:04:01 -0700

    add hover effect

--- src/Project.js
+++ src/Project.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Image } from "react-bootstrap";
 
 const Project = (data) => {
-  const { images, website } = data;
+  const { name, description, images, website } = data;
+  console.log(images);
   return (
     <div className="project-detail">
       <a href={website}>
-        <Image className="project-image" src={images} alt="personal-page" />
+        <div className="corner">#</div>
+        <div className="description">
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+        <img
+          className="project-image"
+          src={require(`${images}`)}
+          alt="personal-page"
+        />
       </a>
     </div>
   );
